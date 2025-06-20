11cbcfd30e5261505deaf8ce9cc0ba652ecc816e
Oraclesid1411 <giloracle76@gmail.com>
2025-06-20 08:48:39 +0000

    update data

--- config/database.js
+++ config/database.js
@@ -3,6 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log("=== VARIABLES DB ===");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? '✅' : '❌');
+console.log("DB_NAME:", process.env.DB_NAME);
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
